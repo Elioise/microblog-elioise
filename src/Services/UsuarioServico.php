bfe4d3b4339b180a8e94c687449ddf4d389ae8d1
Elioise <elioise.rmprevost@senacsp.edu.br>
2025-11-04 16:58:50 -0300

    usuario

--- src/Services/UsuarioServico.php
+++ src/Services/UsuarioServico.php
@@ -3,33 +3,42 @@
 
 //src/Services/UsuarioServico.php
 
-class UsuarioServico {
-      private PDO $conexao;
+class UsuarioServico
+{
+    private PDO $conexao;
 
 
     // Toda vez criamos um objeto baseado na clase UsuarioServico, este objeto  fará ao método de conexao na clase Conecta.
-      public function __construct()
-      {
-            $this->conexao = Conecta::getConexao();
-      }
+    public function __construct()
+    {
+        $this->conexao = Conecta::getConexao();
+    }
 
-      // Métodos CRUD pra usuarios
+    // Métodos CRUD pra usuarios
 
-      // inserir (INSERT)
+    // inserir (INSERT)
 
-      public function inserir( Usuario $dadosUsuario ):void {
-            $sql = "INSERT INTO usuarios(nome, email, tipo, senha)
+    public function inserir(Usuario $dadosUsuario): void
+    {
+        $sql = "INSERT INTO usuarios(nome, email, tipo, senha)
                     VALUES(:nome, :email, :tipo,:senha)";
 
-                    $consulta = $this->conexao->prepare($sql);
+        $consulta = $this->conexao->prepare($sql);
 
-                    $consulta->bindValue(":nome", $dadosUsuario->getNome());
-                     $consulta->bindValue(":email", $dadosUsuario->getEmail());
-                      $consulta->bindValue(":tipo", $dadosUsuario->getTipo());
-                       $consulta->bindValue(":senha", $dadosUsuario->getSenha());
+        $consulta->bindValue(":nome", $dadosUsuario->getNome());
+        $consulta->bindValue(":email", $dadosUsuario->getEmail());
+        $consulta->bindValue(":tipo", $dadosUsuario->getTipo());
+        $consulta->bindValue(":senha", $dadosUsuario->getSenha());
 
-                       $consulta->execute();
+        $consulta->execute();
+    }
 
 
-      }
+    //buscar (SELECT)
+    public function buscar(): array
+    {
+        $sql = "SELECT * FROM usuarios ORDER BY nome";
+        $consulta = $this->conexao->query($sql);
+        return $consulta->fetchAll();
+    }
 }
