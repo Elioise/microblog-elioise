5426cd850311378f2964d8a0a700195f6008419e
Elioise <elioise.rmprevost@senacsp.edu.br>
2025-11-04 17:34:45 -0300

    excercicio

--- admin/usuario-insere.php
+++ admin/usuario-insere.php
@@ -43,8 +43,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			//Executar o serviço e passar os novos dados
 			$usuarioServico->inserir($novoUsuario);
 
-			Utils::redirecionarPara("usuario.php");
-			
+			Utils::redirecionarPara("usuarios.php");
+
 		} catch (Throwable $e) {
 
 			/* Se alguma ação dentro do try falahar, o PHP vai lançar (usando a classe Throwable) um erro/eceção. Ao usar o parâmetro "e" (ou outro nome), temos acesso aos detalhes do que aconteceu. */
--- admin/usuarios.php
+++ admin/usuarios.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once "../src/Database/Conecta.php";
 require_once "../src/Services/UsuarioServico.php";
 require_once "../src/Helpers/Utils.php";
@@ -19,7 +19,7 @@ try {
 
 } catch (Throwable $e) {
 
-	$erro = "Erro ao buscar usuarios.  <br>" .$e->getMessage();
+	$erro = "Erro ao buscar usuarios.  <br>" . $e->getMessage();
 }
 
 
@@ -32,8 +32,8 @@ require_once "../includes/cabecalho-admin.php";
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
-		<h2 class="text-center">Usuários <span class="badge bg-dark">X</span></h2>
+
+		<h2 class="text-center">Usuários <span class="badge bg-dark"><?=count($usuarios)?></span></h2>
 
 		<?php if ($erro): ?>
 			<p class="alert alert-danger text-center"> <?= $erro ?> </p>
@@ -41,12 +41,12 @@ require_once "../includes/cabecalho-admin.php";
 
 		<p class="text-center mt-5">
 			<a class="btn btn-primary" href="usuario-insere.php">
-			<i class="bi bi-plus-circle"></i>	
-			Inserir novo usuário</a>
+				<i class="bi bi-plus-circle"></i>
+				Inserir novo usuário</a>
 		</p>
-				
+
 		<div class="table-responsive">
-		
+
 			<table class="table table-hover">
 				<thead class="table-light">
 					<tr>
@@ -59,34 +59,76 @@ require_once "../includes/cabecalho-admin.php";
 
 				<tbody>
 
-				
+				<?php
+				foreach($usuarios as $usuario):
+
+
+
+				?>
 					<tr>
-						<td> nome do usuário... </td>
-						<td> email do usuário... </td>
-						<td> tipo do usuário... </td>
+						<td> <?=$usuario['nome']?> </td>
+						<td> <?=$usuario['email']?> </td>
+						<td> <?=$usuario['tipo']?> </td>
 						<td class="text-center">
-							<a class="btn btn-warning" 
-							href="usuario-atualiza.php">
-							<i class="bi bi-pencil"></i> Atualizar
+							<a class="btn btn-warning"
+								href="usuario-atualiza.php">
+								<i class="bi bi-pencil"></i> Atualizar
 							</a>
-						
-							<a class="btn btn-danger excluir" 
-							href="usuario-exclui.php">
-							<i class="bi bi-trash"></i> Excluir
+
+							<a class="btn btn-danger excluir"
+								href="usuario-exclui.php">
+								<i class="bi bi-trash"></i> Excluir
 							</a>
 						</td>
 					</tr>
-				
 
-				</tbody>                
+					<?php
+
+						endforeach
+						?>
+
+				</tbody>
 			</table>
-	</div>
-		
+		</div>
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require_once "../includes/rodape-admin.php";
 ?>
 
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Editar fornecedor</title>
+	<link rel="stylesheet" href="../css/estilos.css">
+</head>
+
+<body>
+	<h1>Editar fornecedor</h1>
+
+	<form action="" method="post">
+		<!-- Sempre coloque o código/id do registro de forma oculta
+        no formulário. -->
+		<input type="hidden" name="id" value="<?= $fornecedor['id'] ?>">
+
+		<div>
+			<label for="nome">Nome:</label>
+			<input value="<?= $fornecedor['nome'] ?>" type="text" name="nome" id="nome" required>
+		</div>
+		<button type="submit">Atualizar</button>
+	</form>
+
+	<a href="listar.php">← Voltar</a>
+
+</body>
+
+</html>
